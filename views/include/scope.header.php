02cc19e1b836832df86552af5e19d4af92e6d3f8
julian <jdgiraldo08@misena.edu.co>
2018-06-07 10:16:29 -0500

    wasa4

---
+++ views/include/scope.header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="views/assets/css/master.css">
+    <title>Practicas</title>
+  </head>
+  <body>
+    <section>
