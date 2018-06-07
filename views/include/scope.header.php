c67e34fdad95185dba663cccb0949ed520c80957
Thepahka <scifuentesarango@misena.edu.co>
2018-06-07 10:40:55 -0500

    header

--- views/include/scope.header.php
+++ views/include/scope.header.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="views/assets/css/master.css">
-    <title>Practicas</title>
+    <link rel="shortcut icon" href="views/assets/img/excel.png">
+    <title>Excel</title>
   </head>
   <body>
     <section>
