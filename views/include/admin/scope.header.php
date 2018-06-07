7d04b69211368a709a4faac7ba55f8a791a291bc
julian <jdgiraldo08@misena.edu.co>
2018-06-07 11:23:26 -0500

    r

--- views/include/admin/scope.header.php
+++ views/include/admin/scope.header.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="views/assets/css/master.css">
-    <title>Practicas</title>
+    <link rel="shortcut icon" href="views/assets/img/excel.ico">
+    <link href="https://fonts.googleapis.com/css?family=Cabin:400,700|Roboto:400,700" rel="stylesheet">
+    <title>Excel</title>
   </head>
   <body>
     <section>
--- views/include/scope.header.php
+++ views/include/scope.header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="views/assets/css/master.css">
     <link rel="shortcut icon" href="views/assets/img/excel.ico">
-    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400|Roboto:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cabin:400,700|Roboto:400,700" rel="stylesheet">
     <title>Excel</title>
   </head>
   <body>
