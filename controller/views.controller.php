d2efe3aa031485bf03a146e2b0edbc1ba22915c6
Thepahka <scifuentesarango@misena.edu.co>
2018-06-07 10:46:13 -0500

    views

--- controller/views.controller.php
+++ controller/views.controller.php
@@ -2,6 +2,10 @@
     class ViewsController{
       private $action;
 
+      public function __CONSTRUCT(){
+        // $this->$action->
+      }
+
       public function main(){
         require_once "views/include/scope.header.php";
         require_once "views/modules/index.php";
